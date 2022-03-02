bee97f59d2f6efef54debe5a9e4605e7d92be740
jobs6315 <100549646+jobs6315@users.noreply.github.com>
2022-03-02 22:06:57 +0800

    day2
    加油

---
+++ test.c
@@ -0,0 +1,75 @@
+/*int main()
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		printf("%d ", i);
+		i++;
+
+	}
+	return 0;
+}*/
+/*int main()
+{
+	/*int ch = 0;
+	while ((ch = getchar()) != EOF)
+	{
+		putchar(ch);
+	}*/
+	/*int ret = 0;
+	char password[20] = { 0 };
+	printf("请输入密码：>");
+	scanf_s("%d", password);
+	getchar();
+	printf("请确认（Y/N）：>");
+	ret = getchar();
+	if (ret =='Y')
+	{
+		printf("确认成功\n");
+	}
+	else
+	{
+		printf("放弃确认\n");
+	}
+	return 0;
+}*/
+/*int main()
+{
+	int ch = 0;
+	while ((ch=getchar())!=EOF)
+	{
+		if (ch < '0' || ch>'9')
+			continue;
+		putchar(ch);
+	}
+	return 0;
+}*/
+/*int main()
+{
+	char ch1[10], ch2[10];
+
+	scanf_s("%s", ch1);
+	scanf_s("%s", ch2);
+	printf("%s,%s", ch1, ch2);
+	return 0;
+}*/
+/*int main()
+{
+	int i = 0;
+	for (i = 1; i <= 10; i++)
+	{
+		printf("%d", i);
+	}
+	return 0;
+}*/
+int main()
+{
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		if (i = 5)
+			printf("haha\n");
+		printf("hehe\n");
+	}
+	return 0;
+}
